769481698879c63451f8273b077b0b53d3966361
narutompel <putujackson0@gmail.com>
2023-06-12 01:28:29 +0800

    perubahan

--- masyarakat/lihattanggapan.php
+++ masyarakat/lihattanggapan.php
@@ -9,7 +9,7 @@
                     $data=mysqli_fetch_array($proses);
                     if ($proses) {
                         ?>
-                
+
                     <div class="row">
                         <div class="col-4">
                             <label for="" class="">Nama Pengadu </label>
@@ -22,14 +22,16 @@
                             <label for="" class="">Tanggal Pengaduan </label>
                         </div>
                         <div class="col-8 mt-2">
-                            <input type="text" class="form-control" value="<?php echo date('d-m-Y', strtotime($data['tgl_pengaduan']))?>" readonly>
+                            <input type="text" class="form-control"
+                                value="<?php echo date('d-m-Y', strtotime($data['tgl_pengaduan']))?>" readonly>
                         </div>
 
                         <div class="col-4 mt-2">
                             <label for="" class="">Isi Laporan </label>
                         </div>
                         <div class="col-8 mt-2">
-                            <textarea type="text" class="form-control" rows="6" readonly><?php echo $data['isi_laporan']?></textarea>
+                            <textarea type="text" class="form-control" rows="6"
+                                readonly><?php echo $data['isi_laporan']?></textarea>
                         </div>
                     </div>
                 </div>
@@ -37,44 +39,46 @@
                     <div class="imgtanggapi">
                         <?php if ($data['foto']=="") {
                             ?>
-                            <h2>Tidak Ada Foto</h2>
-                            <?php
+                        <h2>Tidak Ada Foto</h2>
+                        <?php
                         }else {
                             ?>
-                            <div class="imgtanggapi">
+                        <div class="imgtanggapi">
                             <img src="../asset/img/aduan/<?php echo $data['foto']?>" alt="">
-                            </div>
-                            <?php
+                        </div>
+                        <?php
                         }
                         ?>
-                        
-                        
+
+
                     </div>
                 </div>
             </div>
-            <label for="" class=""><h2 style="font-family:'poppins'">Tanggapan</h2></label>
+            <label for="" class="">
+                <h2 style="font-family:'poppins'">Tanggapan</h2>
+            </label>
             <div class="row">
                 <?php 
                     if ($data['status']=="0") {
                     ?>
-                    <h1>Aduan anda sedang di Vertifikasi</h1>
-                    <?php
+                <h1>Aduan anda sedang di Vertifikasi</h1>
+                <?php
                     }elseif ($data['status']=="Proses") {
                         ?>
-                        <h1>Aduan anda sedang di Proses</h1>
-                        <?php
+                <h1>Aduan anda sedang di Proses</h1>
+                <?php
                     }elseif ($data['status']=="Tolak") {
                         ?>
-                        <h1>Aduan anda ditolak karna tidak sesuai dengan kriteria kami!</h1>
-                        <?php
+                <h1>Aduan anda ditolak karna tidak sesuai dengan kriteria kami!</h1>
+                <?php
                     }
                     ?>
-            </div>     
-            <a href="aduan-masyarakat.php" class="btn btn-primary mt-4">Kembali</a>
             </div>
-            <?php
+            <a href="aduan-masyarakat.php" class="btn btn-primary mt-4">Kembali</a>
+        </div>
+        <?php
                 }
             ?>
-            
-        </div>
+
+    </div>
     </div>
--- masyarakat/proses/logout.php
+++ masyarakat/proses/logout.php
@@ -5,8 +5,8 @@ $logout=session_destroy();
 if ($logout) {
 ?>
 <script type="text/javascript">
-    alert ('Anda telah logout');
-    window.location="../../index.php"
+alert('Anda telah logout');
+window.location = "../../index.php"
 </script>
 <?php
 }
